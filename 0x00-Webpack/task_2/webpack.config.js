7b57676717fdf7dfdde6cb91f5030c25242f8d91
LightsNtloko <LightsNtloko@outlook.com>
2024-11-09 04:47:50 +0200

    The wabpack config file

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -10,39 +10,24 @@ module.exports = {
   module: {
     rules: [
       {
-	test: /\.css/,
+	test: /\.css$/,
 	use: ['style-loader', 'css-loader']
       },
       {
 	test: /\.(png|jpg|jpeg|gif)$/i,
 	type: 'asset/resource',
+      },
+      {
+	test: /\/(png|jpe?g|gif)$i,
 	use: [
 	  {
-	    loader: 'file-loader',
+            loader: 'image-webpack-loader',
 	    options: {
-	      name: '[path][name].[ext]',
-	    },
-	  },
-	  {
-	    loader: 'image-webpack-loader',
-	    options: {
-	      mozjpeg: {
-		progressive: true,
-		quality: 65,
-	      },
-	      optipng: {
-		enabled: false,
-	      },
-	      pngquant: {
-		quality: [0.65, 0.90],
-		speed: 4,
-	      },
-	      gitsicle: {
-		 interlaced: false,
-	      },
-	      webp: {
-		quality: 75,
-	      },
+	      mozjpeg: { progressive: true, quality: 65 },
+	      optipng: { enabled: false },
+	      pngquant: { quality: [0.65, 0.90], speed: 4 },
+	      gifsicle: { interlaced: false },
+	      webp: { quality: 75 },
 	    },
 	  },
 	],
