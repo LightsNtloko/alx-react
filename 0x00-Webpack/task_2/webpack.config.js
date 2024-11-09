9ba422ccc01884004035b110815aa345519b2907
LightsNtloko <LightsNtloko@outlook.com>
2024-11-09 04:50:39 +0200

    The wabpack config file

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 	type: 'asset/resource',
       },
       {
-	test: /\.(png|jpe?g|gif)$i,
+	test: /\.(png|jpe?g|gif)$/i,
 	use: [
 	  {
             loader: 'image-webpack-loader',
