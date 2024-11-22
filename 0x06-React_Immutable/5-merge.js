6cc9e40da3380d692898d1a05788d15705bbd63d
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-11-22 03:13:19 +0000

    function named concatElements and a function named mergeElements

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,12 +1,5 @@
 import { List, Map } from 'immutable';
 
-export function concatElements(page1, page2) {
-  return List(page1).concat(page2);
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-export function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-  const mergedMap = map1.merge(map2);
-  return List(mergedMap.values());
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
