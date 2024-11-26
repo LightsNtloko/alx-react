c1bb80f10de93a7332aac27589af4d5ef0fa2e54
Khanyiso Ntloko <122798170+LightsNtloko@users.noreply.github.com>
2024-11-26 14:10:16 +0200

    Delete 0x01-react_intro/task_1 directory

--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-
-export function getFullYear() {
-  return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-  return isIndex ? "Holberton Schoo;" : "Holberton school main dashboard";
-}
