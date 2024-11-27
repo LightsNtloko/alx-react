ac63523ac72e5cdff2f115a67c14319d2be4cb0c
LightsNtloko <LightsNtloko@outlook.com>
2024-11-27 17:36:27 +0200

    App main js file

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './holberton-logo.jpg';
 import './App.css';
 
@@ -5,13 +6,15 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="Holberton logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">Copyright 2020 - holberton School</div>
+      <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
     </div>
   );
 }
