b31256607b554cf460a418ccd538fe76baf1d393
LightsNtloko <ST10125711@rcconnect.edu.za>
2025-01-29 12:05:21 +0000

    Update your tests

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -9,12 +9,63 @@ import uiReducer, { initialState } from "../reducers/uiReducer";
 const store = createStore(uiReducer, initialState);
 
 describe("<App />", () => {
-  it("mapStateToProps returns the right object from user login state", () => {
+  it("mapStateToProps returns the correct object from user login state", () => {
     let state = fromJS({
       isUserLoggedIn: true,
     });
 
     const expected = { isLoggedIn: true };
     expect(mapStateToProps(state)).toEqual(expected);
+  });
+
+  it("mapStateToProps returns the correct object from notification drawer state", () => {
+    let state = fromJS({
+      isNotificationDrawerVisible: true,
+    });
+
+    const expected = { displayDrawer: true };
+    expect(mapStateToProps(state)).toEqual(expected);
+  });
+
+  it("renders the <App /> component correctly when logged in", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <App isLoggedIn={true} displayDrawer={false} />
+      </Provider>
+    );
+
+    expect(wrapper.find("CourseList").exists()).toBe(true);
+    expect(wrapper.find("Login").exists()).toBe(false);
+  });
+
+  it("renders the <App /> component correctly when logged out", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <App isLoggedIn={false} displayDrawer={false} />
+      </Provider>
+    );
+
+    expect(wrapper.find("CourseList").exists()).toBe(false);
+    expect(wrapper.find("Login").exists()).toBe(true);
+  });
+
+  it("should render notifications drawer when displayDrawer is true", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <App isLoggedIn={true} displayDrawer={true} />
+      </Provider>
+    );
+
+    expect(wrapper.find("Notifications").prop("displayDrawer")).toBe(true);
+  });
+
+  it("should not render notifications drawer when displayDrawer is false", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <App isLoggedIn={true} displayDrawer={false} />
+      </Provider>
+    );
+
+    expect(wrapper.find("Notifications").prop("displayDrawer")).toBe(false);
   });
 });
