d9682c9b131d7366588b9f520df1849f9314e2fc
LightsNtloko <ST10125711@rcconnect.edu.za>
2025-01-29 10:13:18 +0000

    Create a small store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App/App";
+import uiReducer from "./reducers/uiReducer";
+
+// Create Redux store with uiReducer
+const store = createStore(uiReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
